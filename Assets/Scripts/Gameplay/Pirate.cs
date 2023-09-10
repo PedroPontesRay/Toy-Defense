a1bab8d52e7bcc6ba15c08a9f8b6f086b5555240
Pedro Henrique <113391251+PedroPontesRay@users.noreply.github.com>
2023-09-10 14:53:38 -0300

    merge

--- Assets/Scripts/Gameplay/Pirate.cs
+++ Assets/Scripts/Gameplay/Pirate.cs
@@ -1,11 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pirate : MonoBehaviour
 {
 
     [SerializeField] private float rangeFire;
+    private float damage;
+
+    public event Action<Enemy> OnContinuousAttack = delegate { };
+
+    private void Start()
+    {
+        InvokeRepeating("CheckForEnemies", 0f, 1f);
+    }
+
+
+    private void CheckForEnemies()
+    {
+        
+        Collider[] enemyGonDamage = Physics.OverlapSphere(transform.position, rangeFire);
+        foreach(Collider collider in enemyGonDamage)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Enemy enemy = collider.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    EnemyTakingDamage(enemy);
+                }
+            }
+        }
+    }
+
+
+
+
+    private void EnemyTakingDamage(Enemy enemy)
+    {
+        OnContinuousAttack.Invoke(enemy);
+    }
+    public void StopEnemyTakingDamage()
+    {
+        OnContinuousAttack.Invoke(null);
+    }
+
+
+
+    
 
 
     private void OnDrawGizmos()
